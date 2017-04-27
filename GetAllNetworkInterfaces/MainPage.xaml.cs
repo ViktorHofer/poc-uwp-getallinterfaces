9b6f9da0529acb74988ae77a354e29f7aa04338b
Viktor Hofer <viktor.hofer@microsoft.com>
2017-04-27 16:23:16 +0200

    Sample push

---
+++ GetAllNetworkInterfaces/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Sockets.Plugin;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GetAllNetworkInterfaces
+{
+	public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var allInterfaces = await CommsInterface.GetAllInterfacesAsync();
+		}
+	}
+}
---
+++ GetAllNetworkInterfaces/README.txt
@@ -0,0 +1,5 @@
+﻿Look into MainPage.xaml.cs
+https://github.com/rdavisau/sockets-for-pcl
+
+For HambugerMenu consider UWP Community Toolkit 
+https://github.com/Microsoft/UWPCommunityToolkit
